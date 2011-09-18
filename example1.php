d9817a56bcbc308e8e89edb43e022d7c92f16e71
wcode404@gmail.com <wcode404@gmail.com@1fa3d28c-2777-26e1-ef74-31f98554772f>
2011-09-18 18:26:27 +0000

    Добавлены 3 примера работы с подробными комментариями.

--- example1.php
+++ example1.php
@@ -1,1 +1,34 @@
 <?php
+use Paged\Facade;
+
+require_once 'Facade.php';
+
+try {
+
+	// устанавливаем язык
+	Facade::setLangID('ru');
+
+	// инициализируем и указываем номер последней страницы
+	$menu = Facade::getMenu()->setLast(15);
+
+	// накладываем базовый вид и отрисовываем меню
+	Facade::setView($menu)->showPack();
+
+} catch (Exception $e){ // обработчик ошибок
+	exit('Error: '.$e->getMessage());
+}
+
+/**
+ * Результат выполнения
+ * 
+ * <span>1</span>
+ * <a href="?page=2" title="Страница 2">2</a>
+ * <a href="?page=3" title="Страница 3">3</a>
+ * <a href="?page=4" title="Страница 4">4</a>
+ * <a href="?page=5" title="Страница 5">5</a>
+ * <a href="?page=6" title="Страница 6">6</a>
+ * <a href="?page=7" title="Страница 7">7</a>
+ * <a href="?page=8" title="Страница 8">8</a>
+ * <a href="?page=9" title="Страница 9">9</a>
+ * <a href="?page=10" title="Страница 10">10</a>
+ */
---
+++ example3.php
@@ -0,0 +1,53 @@
+<?php
+use Paged\Facade;
+
+require_once 'Facade.php';
+
+try {
+
+	// устанавливаем язык
+	Facade::setLangID('ru');
+
+	// инициализируем
+	$menu = Facade::getMenu()
+		// указываем номер последней страницы
+		->setListLength(5)
+		// указываем название переменной
+		->setVariable('p')
+		// указываем длину списка
+		->setLast(15);
+
+	// приводит ссылки к абсолютным
+	$view = Facade::setView($menu, 'Links');
+	// добавляет ссылки на следующую и предыдущую страницу
+	$view = Facade::setView($view, 'PreviousNext');
+	// добавляет ссылки на первую и последнюю страницу
+	$view = Facade::setView($view, 'FerstLast');
+	// отображает результат в списке ul, li
+	$view = Facade::setView($view, 'List');
+	// кэшируем вывод. обработка данных и отработка шаблона выполняется только один раз
+	// повторные вызовы вывода данных будут возвращать данные из кэша
+	$view = Facade::setView($view, 'Cache');
+
+	// отрисовываем меню
+	$view->showPack();
+
+} catch (Exception $e){ // обработчик ошибок
+	exit('Error: '.$e->getMessage());
+}
+
+/**
+ * Результат выполнения
+ * 
+ * <ul>
+ * 	<li><span>Первая</span></li>
+ * 	<li><span>Предыдущая</span></li>
+ * 	<li><span>1</span></li>
+ * 	<li><a href="http://paged/?p=2" title="Страница 2">2</a></li>
+ * 	<li><a href="http://paged/?p=3" title="Страница 3">3</a></li>
+ * 	<li><a href="http://paged/?p=4" title="Страница 4">4</a></li>
+ * 	<li><a href="http://paged/?p=5" title="Страница 5">5</a></li>
+ * 	<li><a href="http://paged/?p=2" title="Следующая страница">Следующая</a></li>
+ * 	<li><a href="http://paged/?p=15" title="Последняя страница">Последняя</a></li>
+ * </ul>
+ */
